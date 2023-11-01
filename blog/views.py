8c1e43ebee9a7b8e837adac5757c209536ffad79
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-01 09:36:13 +0100

    style: css files and images for 'index.html' and 'all_posts.html'

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def starting_page(request: HttpRequest) -> HttpResponse:
+    return render(request, 'blog/index.html')
+
+
+def posts(request: HttpRequest) -> HttpResponse:
+    return render(request, 'blog/all_posts.html')
+
+
+def post_detail(request: HttpRequest, slug: str) -> HttpResponse:
+    pass
