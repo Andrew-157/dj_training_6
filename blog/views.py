961ecdb99dfa0dcc61536d994ce6c9b34a99f0d5
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-13 11:22:40 +0100

    feat: use models for all posts page and post detail page

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,7 @@
-from datetime import date
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 
 from .models import Post, Author, Tag
-
-posts_data = []
-
-
-def get_post_date(post: {}):
-    return post['date']
 
 
 def starting_page(request: HttpRequest) -> HttpResponse:
@@ -19,11 +11,12 @@ def starting_page(request: HttpRequest) -> HttpResponse:
 
 
 def posts(request: HttpRequest) -> HttpResponse:
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, 'blog/all_posts.html',
-                  context={'all_posts': posts_data})
+                  context={'all_posts': all_posts})
 
 
 def post_detail(request: HttpRequest, slug: str) -> HttpResponse:
-    identified_post = next(post for post in posts_data if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post_detail.html',
                   context={'post': identified_post})
