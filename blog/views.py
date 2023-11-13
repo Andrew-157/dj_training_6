56c0e6544f8467b97aa40e30e3082a282348273d
Andrew Subotin <subotinandrey5@gmail.com>
2023-11-13 11:11:17 +0100

    feat: load data to the index page using models

--- blog/views.py
+++ blog/views.py
@@ -3,71 +3,9 @@ from datetime import date
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+from .models import Post, Author, Tag
 
-posts_data = [
-    {"slug": "hike-in-the-mountains",
-     "image": "mountains.jpg",
-     "author": "Andrew-157",
-     "date": date(2023, 7, 14),
-     "title": "Mountain Hiking",
-     "excerpt": """Mountain Hiking is amazing activity.
-                    It is good for both your physical and mental health.""",
-     "content": """ 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, delectus.
-        Corrupti repellendus magni, assumenda eos provident officiis ipsam recusandae ad,
-        necessitatibus quae dolore aliquid, temporibus totam fugit dicta officia porro!
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, delectus.
-        Corrupti repellendus magni, assumenda eos provident officiis ipsam recusandae ad,
-        necessitatibus quae dolore aliquid, temporibus totam fugit dicta officia porro!
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, delectus.
-        Corrupti repellendus magni, assumenda eos provident officiis ipsam recusandae ad,
-        necessitatibus quae dolore aliquid, temporibus totam fugit dicta officia porro!       
-        """},
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Andrew-157",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Andrew-157",
-        "date": date(2023, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-]
+posts_data = []
 
 
 def get_post_date(post: {}):
@@ -75,8 +13,7 @@ def get_post_date(post: {}):
 
 
 def starting_page(request: HttpRequest) -> HttpResponse:
-    sorted_posts = sorted(posts_data, key=get_post_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, 'blog/index.html',
                   context={'posts': latest_posts})
 
